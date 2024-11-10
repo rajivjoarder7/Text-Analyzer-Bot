59b0fbe59c775570f1409bea654097ae58f4022a
Rajiv Joarder <75382716+rajivjoarder7@users.noreply.github.com>
2024-11-10 07:27:29 +0530

    Update text_summarizer.py

--- Codes/Scripts/text_summarizer.py
+++ Codes/Scripts/text_summarizer.py
@@ -8,7 +8,7 @@ device = 0 if torch.cuda.is_available() else -1  # -1 for CPU, 0 for GPU
 model_name = "facebook/bart-large-cnn"
 summarizer = pipeline("summarization", model=model_name, device=device)
 
-def summarize_text(text, summary_ratio=0.3):
+def summarize_text(text, summary_ratio=0.42):
     try:
         summary = summarizer(text, max_length=int(len(text.split()) * summary_ratio), min_length=50, do_sample=False)
         return summary[0]['summary_text']
