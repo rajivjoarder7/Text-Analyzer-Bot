62350f838740f98a9959c688aa4b2c8360f71ca8
Rajiv Joarder <75382716+rajivjoarder7@users.noreply.github.com>
2024-11-20 16:44:32 +0530

    Update qa_bot.py

--- Codes/Scripts/qa_bot.py
+++ Codes/Scripts/qa_bot.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 # Load pre-trained T5 model fine-tuned on Question Answering
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model_name = "t5-large"  # Use large T5 model, fine-tuned QA-specific versions can be tried as well
+model_name = "t5-large"  
 tokenizer = T5Tokenizer.from_pretrained(model_name)
 model = T5ForConditionalGeneration.from_pretrained(model_name)
 model.to(device)
